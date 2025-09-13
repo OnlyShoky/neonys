7913edd55fbaa0b649791237c314c414e6fd0c44
Shoky <leozul@hotmail.es>
2025-09-13 11:12:00 +0200

    Added get this agent functionality

--- src/app/shared/components/agent-card/agent-card.ts
+++ src/app/shared/components/agent-card/agent-card.ts
@@ -1,6 +1,12 @@
 import { Component, Input, Output, EventEmitter, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslationService } from '../../../core/services/translation';
+import { ScrollService } from '../../../../../src/app/core/services/scroll';
+import { DOCUMENT, isPlatformBrowser } from '@angular/common';
+import {  Inject, PLATFORM_ID } from '@angular/core';
+
+
+
 
 export interface Agent {
   name: string;
@@ -24,6 +30,13 @@ export interface Agent {
 export class AgentCardComponent {
   @Input() agent!: Agent;
   @Output() getAgent = new EventEmitter<Agent>();
+  private scrollService = inject(ScrollService);
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
+  
 
   private translationService = inject(TranslationService);
 
@@ -33,6 +46,21 @@ export class AgentCardComponent {
 
   onGetAgent(): void {
     this.getAgent.emit(this.agent);
+  }
+
+    scrollToSection(sectionId: string): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const element = this.document.getElementById(sectionId);
+      if (element) {
+        const headerHeight = 80; // Ajusta según la altura de tu navbar
+        const targetPosition = element.getBoundingClientRect().top + window.pageYOffset - headerHeight;
+        
+        window.scrollTo({
+          top: targetPosition,
+          behavior: 'smooth'
+        });
+      }
+    }
   }
 }
 
